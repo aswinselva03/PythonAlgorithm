c627ce26d7ee310085ef9f4ad9b96fe8cf4ebc31
aswinselva03 <36582181+aswinselva03@users.noreply.github.com>
2023-04-04 05:16:31 +0000

    updated

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -1,3 +1,50 @@
 class Node:
-    def __init__(self):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+    
+    def __str__(self):
+        linked_list = ""
+        if self.head == None:
+            return "None"
+        for _ in range(0, self.length+1):
+            
+            linked_list += str(self.head.value)
+            linked_list += "->"
+            if self.head.next == None:
+                linked_list += "None"
+                return linked_list
+            self.head = self.head.next
+        return linked_list
+            
+    
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+    def prepend(self, value):
+        pass
+
+    def insert(self, position, value):
+        pass
+    
+    def remove(self, position, value):
+        pass
+
+my_linked_list = LinkedList(4)
+my_linked_list.append(2)
+print(my_linked_list)
         
