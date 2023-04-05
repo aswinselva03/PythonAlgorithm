ccf7ff3fdb933801597e6d64ae3a7a4826cb2f98
aswinselva03 <36582181+aswinselva03@users.noreply.github.com>
2023-04-05 04:35:21 +0000

    add  pop

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -2,6 +2,10 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
+    
+    def __str__(self):
+        return str({"value":self.value,
+                    "next": self.next})
 
 class LinkedList:
 
@@ -28,12 +32,39 @@ class LinkedList:
     
     def append(self, value):
         new_node = Node(value)
+        # if the linked list is empty assign head and tail to new node
         if self.head is None:
             self.head = new_node
             self.tail = new_node
+        # if not, assign, tail.next  as new node(now None) and then shift tail to the new node(already new_node.next is None)
         else:
             self.tail.next = new_node
             self.tail = new_node
+        # increment the lenght of LL and return True(will be used in another function)
+        self.length +=1
+        return True
+    
+    def pop(self):
+        # if length is 0, return None
+        if self.length == 0:
+            return None
+        # we are using pre and temp, to find the node before tail (if doubly linked list we can do a prev in tail to get that)
+        # here we need to do it ourselves
+        pre = self.head
+        temp = self.head
+        # our goal is to find new tail(since pop removes the last node, node before last node is our new tail)
+        while(temp.next): # until temp.next is not none - temp is not the tail
+            prev = temp # retaining the previous node of tail in prev
+            temp = temp.next
+        prev.next = None
+        self.tail = prev
+        self.length -= 1
+        # for cases when we have only one node in LL, the node will not be removed from above steps so
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp # the last element removed should be returned
+
 
     def prepend(self, value):
         pass
@@ -45,6 +76,10 @@ class LinkedList:
         pass
 
 my_linked_list = LinkedList(4)
+print("appending 2")
 my_linked_list.append(2)
 print(my_linked_list)
+print("popping 2",my_linked_list.pop())
+print(my_linked_list)
+
         
