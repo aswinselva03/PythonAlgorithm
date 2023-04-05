f104edcac8411d5496d96a533556cc3e4d9f87c6
aswinselva03 <36582181+aswinselva03@users.noreply.github.com>
2023-04-05 19:36:00 +0000

    linked list complete

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -4,9 +4,8 @@ class Node:
         self.next = None
     
     def __repr__(self):
-        return str({"value":self.value,
-                    "next": self.next})
-    
+        return str("Node")
+
     def __str__(self):
         return str({"value":self.value,
                     "next": self.next})
@@ -98,19 +97,72 @@ class LinkedList:
         return temp
     
     def get(self,index):
+        # if out of bound
         if index < 0 or index >= self.length:
             return None
+        # we need to return node so assign head to temp and loop to get to the index position
         temp = self.head
         for _ in range(index):
             temp = temp.next
         return temp
 
+    def set(self, index, value):
+        # use get to get the node at index, if node is not none, set the value
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
 
-    def insert(self, position, value):
-        pass
+    def insert(self, index, value):
+        # index out of bound
+        if index < 0 or index>self.length:
+            return False
+        # when you have to insert as the first node, do prepend
+        if index == 0:
+            return self.prepend(value)
+        # when you have to insert as the last node, do append
+        if index == self.length:
+            return self.append(value)
+        # Any other position, get the node before index position and swap next values
+        new_node = Node(value)
+        temp = self.get(index -1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+
     
-    def remove(self, position, value):
-        pass
+    def remove(self, index):
+        if index < 0 or index>=self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        prev = self.get(index - 1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+    
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+
+
+
+
+        
 
 my_linked_list = LinkedList(4)
 print("appending 2")
@@ -129,4 +181,15 @@ print("appending 4", my_linked_list.append(4))
 print(my_linked_list)
 print("Get index 2",my_linked_list.get(2))
 print("Get index 0",my_linked_list.get(0))
-        
+
+print("set index 0",my_linked_list.set(0,5))
+print(my_linked_list)
+print("set index 2",my_linked_list.set(2,5))
+print(my_linked_list)
+my_linked_list.insert(0, 3)
+my_linked_list.insert(2,1)
+print(my_linked_list)
+my_linked_list.remove(0)
+print(my_linked_list)
+print(my_linked_list.reverse())
+print(my_linked_list)
